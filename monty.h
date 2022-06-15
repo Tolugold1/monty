5873b5d70e5b0749ebc28b79abce4eeb0c2efa5c
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-06-15 06:22:21 -0700

    updates

--- monty.h
+++ monty.h
@@ -17,9 +17,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -32,14 +32,22 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 stack_t *addNode(stact_t **stack, unsigned int n);
 size_t print_all_stack(const stack_t *stack);
 void freeStack(stack_t *stack);
 
+/**
+ * struct assist - argument for the opcode
+ * @n_dataStruct: stack default mode
+ * @arg: the string argument
+ *
+ * Description: global structure used to pass
+ * data around the functions
+ */
 typedef struct assist
 {
 	int n_dataStruct;
